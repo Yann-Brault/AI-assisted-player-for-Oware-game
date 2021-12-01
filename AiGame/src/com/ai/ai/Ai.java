62643343f966cd66620fc18efa7addb42094cd68
Antoine <monsieur.cousson@gmail.com>
2021-12-01 11:36:46 +0100

    test

--- AiGame/src/com/ai/ai/Ai.java
+++ AiGame/src/com/ai/ai/Ai.java
@@ -12,13 +12,13 @@ public class Ai {
 
     public int evaluate(Position posCourante, boolean iaTurn, int Profondeur) {
 //        System.out.println("hello " +(posCourante.getPionsPrisOrdi() - posCourante.getPionsPrisJoueur()) );
-//        return posCourante.getPionsPrisOrdi() - posCourante.getPionsPrisJoueur();
-            if(iaTurn){
-                return posCourante.getPionsPrisOrdi();
-            }
-            else{
-                return posCourante.getPionsPrisJoueur();
-            }
+              return posCourante.getPionsPrisOrdi() - posCourante.getPionsPrisJoueur();
+           // if(iaTurn){
+             //   return posCourante.getPionsPrisOrdi();
+            //}
+            //else{
+              //  return posCourante.getPionsPrisJoueur();
+            //}
     }
 
     public int valeurMinMax(Position posCourante, boolean iaTurn, int profondeur, int profondeurMax) {
